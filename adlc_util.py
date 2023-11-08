dcf5e2ad7a45a37b13738e2b04ebbe45e78b4916
abcoste2 <abcoste2@ncsu.edu>
2023-11-07 19:53:18 -0500

    image displaying

--- adlc_util.py
+++ adlc_util.py
@@ -1,4 +1,7 @@
 from keras_cv import visualization
+from keras_cv import bounding_box
+
+CLASS_MAPPING = {0:"target"}
 
 def visualize_dataset(inputs, value_range, rows, cols, bounding_box_format, offset=1):
     """
@@ -20,13 +23,19 @@ def visualize_dataset(inputs, value_range, rows, cols, bounding_box_format, offs
         line_thickness=1,
         font_scale=0.7,
         bounding_box_format=bounding_box_format,
-        class_mapping={0: "target"},
+        class_mapping=CLASS_MAPPING,
     )
 
-def visualize_detections(model, dataset, bounding_box_format):
-    images, y_true = next(iter(dataset.take(1)))
+def visualize_detections(model, dataset, bounding_box_format, offset=2):
+    # images, y_true = next(iter(dataset.take(1)))
+
+    it = iter(dataset.take(offset))
+
+    for _ in range(offset):
+        images, y_true = next(it)
+
     y_pred = model.predict(images)
-    y_pred = keras_cv.bounding_box.to_ragged(y_pred)
+    y_pred = bounding_box.to_ragged(y_pred)
     visualization.plot_bounding_box_gallery(
         images,
         value_range=(0, 255),
@@ -38,5 +47,5 @@ def visualize_detections(model, dataset, bounding_box_format):
         cols=4,
         show=True,
         font_scale=0.7,
-        class_mapping=class_mapping,
+        class_mapping=CLASS_MAPPING,
     )
